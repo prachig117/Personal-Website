3fe161a3740ec13a7b16bb7473d4036961faa09e
Prachi Gupta <prachigupta@Prachis-MacBook-Pro.local>
2018-09-20 22:18:51 -0400

    first draft

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $('#name').css('padding-top',''+($(window).height())/2 - 83+'px');
+
+
+});
+$(window).resize(function(){
+  $('#name').css('padding-top',''+($(window).height())/2 - 83+'px');
+});
