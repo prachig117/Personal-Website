77516648d7b72a00f2a3eef2c2a1c9fcf0cbaad2
Prachi Gupta <prachigupta@Prachis-MacBook-Pro.local>
2018-09-21 01:14:26 -0400

    main page done, need to do portfolio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+  $('#fullpage').fullpage({
+		//options here
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+		autoScrolling:true,
+		scrollHorizontally: true
+	});
 
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
   $('#name').css('padding-top',''+($(window).height())/2 - 83+'px');
 
 
