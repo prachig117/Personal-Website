501c7efb7555add68b2a1690a440a8365ea44337
prachig117 <p29gupta@edu.uwaterloo.ca>
2019-01-16 00:46:56 -0500

    3 different responsive commits

--- js/script.js
+++ js/script.js
@@ -9,12 +9,26 @@ $(document).ready(function(){
 	//methods
 	$.fn.fullpage.setAllowScrolling(false);
   $('#name').css('padding-top',''+($(window).height())/2 - 83+'px');
-  $('.main-name').css('padding-top',''+($(window).height())/2 - 143+'px');
+  $('.main-name').css('padding-top',''+($(window).height())/2 - 123+'px');
+  $('#menu-open-button').click(function() {
+    $('.menu').fadeIn();
+    $('.menu-icon').fadeOut();
+    $('.container').css('overflow','hidden');
+  });
+  $('#menu-close-button').click(function() {
+    $('.menu').fadeOut();
+    $('.menu-icon').fadeIn();
+    $('.container').css('overflow','scroll');
+  });
+  if ($(window).width() < 960) {
+    $('.right-scroll').removeAttr('id');
+    $('.right-scroll').removeClass('fullpage-wrapper');
 
+  } else {
+    $('.right-scroll').attr('id','fullpage');
+  }
 
 });
 $(window).resize(function(){
-  $('#name').css('padding-top',''+($(window).height())/2 - 83+'px');
-  $('.main-name').css('padding-top',''+($(window).height())/2 - 143+'px');
-
+  $('.main-name').css('padding-top',''+($(window).height())/2 - 123+'px');
 });
