ba89e1bea92e9b4aef5d84eaa0d67e5c926ac5fb
Prachi Gupta <prachigupta@Prachis-MacBook-Pro-2.local>
2019-01-15 23:31:12 -0500

    Adding menu links

--- js/script.js
+++ js/script.js
@@ -9,7 +9,16 @@ $(document).ready(function(){
 	//methods
 	$.fn.fullpage.setAllowScrolling(false);
   $('#name').css('padding-top',''+($(window).height())/2 - 83+'px');
-
+  $('#menu-open-button').click(function() {
+    $('.menu').fadeIn();
+    $('.menu-icon').fadeOut();
+    $('.container').css('overflow','hidden');
+  });
+  $('#menu-close-button').click(function() {
+    $('.menu').fadeOut();
+    $('.menu-icon').fadeIn();
+    $('.container').css('overflow','scroll');
+  });
 
 });
 $(window).resize(function(){
